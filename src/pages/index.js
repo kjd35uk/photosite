17e78f6dcc556be8545904dd472238b58624559a
kirstiedavidson <kirstie.davidson@onthebeach.co.uk>
2019-05-07 15:02:02 +0100

    Setting up cover photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    {/* const fluid = data.images.edges[0].node.childImageSharp.fluid */}
+    <Img alt="" fluid={data.images.edges[1].node.childImageSharp.fluid}/>
+      {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/fujian-2017">Fujian 2017</Link>
+    <Link to="/margarita-rumble-2019">Margarita Rumble 2019</Link>
+    <Link to="/portraits">Portraits</Link>
+    <Link to="/malta-2016">Malta 2017</Link>
+    <Link to="/manchester">Manchester Street Photography</Link>
   </Layout>
 )
 
+export const query = graphql`
+query allCoverImgs {
+  images: allFile(
+    filter: { relativePath: { regex: "/coverphotos/.*.jpg/" } }
+    ) {
+      edges{
+        node{
+          relativePath
+          name
+          childImageSharp {
+            fluid(maxWidth: 9300, maxHeight: 5860, quality:90) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+  }
+}
+`;
 export default IndexPage
---
+++ src/pages/margarita-rumble-2019.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MargaritaRumble = () => (
+  <Layout>
+    <SEO title="Margarita Rumble 2019 photos" />
+    <p>Welcome to Margarita Rumble 2019</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default MargaritaRumble
