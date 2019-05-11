aecfb2de1f41aeb7e7eba74b38cb445a5d640825
Kirstie Davidson <kjd35uk@yahoo.co.uk>
2019-05-11 15:30:52 +0100

    WIP2

---
+++ src/pages/50th-party.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "gatsby";
+import Img from 'gatsby-image'
+import { graphql } from 'gatsby'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FiftiethParty = ({ data }) => (
+  <Layout>
+    <SEO title="Malta Black and White Protography" />
+    <div className="main-div">
+      <div className="album-name">Fujian 2017</div>
+        <div className="album-pics">
+          {data.images.edges.map( image => {
+            const name = image.node.name
+            const title = name.replace(/_/g, " ").split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ');
+            return(
+              <div key={image.node.name}>
+                <Img alt="" fluid={image.node.childImageSharp.fluid}/>
+                <p>{title}</p>
+              </div>
+            )
+          })}
+        </div>
+    </div>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export const query = graphql`
+query FiftiethPartyImgs {
+  images: allFile(
+    sort: { order: ASC, fields: [absolutePath] }
+    filter: { relativePath: { regex: "/fujian/.*./" } }
+    ) {
+      edges{
+        node{
+          relativePath
+          name
+          childImageSharp {
+            fluid(maxWidth: 4000, maxHeight: 2700, quality:90) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+  }
+}
+`;
+
+export default FiftiethParty
